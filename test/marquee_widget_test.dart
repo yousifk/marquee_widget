a9b5fb5aa1a18c1bac12682eac867bbd9769ab6e
Yousif Khalid <yousifk55@gmail.com>
2019-05-10 10:55:21 +0300

    0.0.1

--- test/marquee_widget_test.dart
+++ test/marquee_widget_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
+//import 'package:flutter_test/flutter_test.dart';
 
-import 'package:marquee_widget/marquee_widget.dart';
+//import 'package:marquee_widget/marquee_widget.dart';
 
 void main() {
  /* test('adds one to input values', () {
